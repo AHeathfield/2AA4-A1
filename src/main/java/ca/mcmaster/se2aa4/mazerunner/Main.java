adb054ff21302edfda14dde84eddc3f2e100f81c
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-01-24 15:05:21 +0000

    Feature #9: Added class IOManager to handle input and outputs

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -16,42 +16,17 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+        IOManager ioManager = new IOManager(args);
         // System.out.println("** Starting Maze Runner");
         logger.info("** Starting Maze Runner");
         try {
-            // Creating Option (using Apache CLI)
-            Options options = new Options();
-
-            // add i option
-            options.addOption("i", true, "Gets value of the command right after -i");
-
-            CommandLineParser parser = new DefaultParser();
-            CommandLine cmd = parser.parse(options, args);
-
-            if (cmd.hasOption("i")) {
-                String file = cmd.getOptionValue("i");
-                // System.out.println("**** Reading the maze from file " + file);
-                logger.info("**** Reading the maze from file {}", file);
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    for (int idx = 0; idx < line.length(); idx++) {
-                        if (line.charAt(idx) == '#') {
-                            System.out.print("WALL ");
-                        } else if (line.charAt(idx) == ' ') {
-                            System.out.print("PASS ");
-                        }
-                    }
-                    System.out.print(System.lineSeparator());
-                }
-            }
+            System.out.println(ioManager.getFilePath());
+            System.out.println(ioManager.getUserPath());
             
         } catch(Exception e) {
             System.err.println("/!\\ An error has occured /!\\");
         }
-        // System.out.println("**** Computing path");
-        // System.out.println("PATH NOT COMPUTED");
-        // System.out.println("** End of MazeRunner");
+  
         logger.info("**** Computing path");
         logger.info("PATH NOT COMPUTED");
         logger.info("** End of MazeRunner");
