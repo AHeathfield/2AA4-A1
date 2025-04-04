10833fee2c95046f43b3eb51cde935f5b49693d1
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-04-04 19:16:16 -0400

    Slight refactor on Path class to declare runner as the interface Runner and not the concrete MazeRunner

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
  */
 public class Path {
     private static final Logger logger = LogManager.getLogger();
-    private MazeRunner runner;
+    private Runner<Direction, Instruction> runner;
     private String path;
 
     // Contructor
